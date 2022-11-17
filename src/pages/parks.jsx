d1592de3c6268183413fe0f420d22fb8d404e00d
Cobi Reeves <cobi.reeves@gmail.com>
2022-11-17 12:36:37 -0700

    initial park layout

--- src/pages/parks.jsx
+++ src/pages/parks.jsx
@@ -4,7 +4,12 @@ import Weather from '../components/WeatherAPI/Weather'
 const Parks = () => {
   return (
     <header className="App-header">
-      < Weather />
+      <div id='parks-container'>
+        <div id='park-display'>Parks List</div>
+        <div id='search-box'>Search</div>
+        <div id='map-display'>Map</div>
+        <div id='weather-display'>< Weather /></div>
+      </div>
       </header>
   )
 }
