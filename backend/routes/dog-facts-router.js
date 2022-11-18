0d74af62bf5953c822b03577658bc2405abb6947
Cobi Reeves <cobi.reeves@gmail.com>
2022-11-18 10:16:34 -0700

    quick try-catch to dog facts

--- backend/routes/dog-facts-router.js
+++ backend/routes/dog-facts-router.js
@@ -4,10 +4,15 @@ import fetch from "node-fetch";
 const router = Router()
 
 router.get('/', async(req, res) => {
+    try{
     let serverReq = await fetch(`http://dog-api.kinduff.com/api/facts`);
     let fact = await serverReq.json();
     console.log("fact sent");
     res.send(fact.facts);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send();
+      }
 })
 
 export default router
