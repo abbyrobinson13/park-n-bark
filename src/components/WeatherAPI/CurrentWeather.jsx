1ae5dbf64ec8a1f8a241edafb281c271be70c7a1
Abneet K Pabyal <Abneet.P@gmail.com>
2022-11-22 20:30:22 -0700

    CSS on Parks page and fiddled with tailwind

--- src/components/WeatherAPI/CurrentWeather.jsx
+++ src/components/WeatherAPI/CurrentWeather.jsx
@@ -3,10 +3,10 @@ const CurrentWeather = ({ currentWeather }) => {
   let icon = `http://openweathermap.org/img/wn/${currentWeather.icon}@2x.png`
   return (
     <div id="weather-display">
-      <div> <img src={icon} alt="icon"/>{currentWeather.weather} </div>
-      <div> Conditions: {currentWeather.conditions} </div>
-      <div> Temperature: {currentWeather.temperature}&deg;C </div>
-      <div> Feels Like: {currentWeather.feelslike}&deg;C</div>
+      <div id= "icon"> <img src={icon} alt="icon"/>{currentWeather.weather} </div>
+      <div className="weather-info"> Conditions: <div> {currentWeather.conditions}</div> </div>
+      <div className="weather-info"> Temperature: <div> {currentWeather.temperature}&deg;C </div> </div>
+      <div className="weather-info"> Feels Like: <div> {currentWeather.feelslike}&deg;C</div> </div>
     </div>
   );
 };
