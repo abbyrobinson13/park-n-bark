6a6e3c827416e11f084f5ba6d6d97f6dd22396cf
Abneet K Pabyal <Abneet.P@gmail.com>
2022-11-22 20:45:30 -0700

    formatting and restructure of weather

--- src/components/WeatherAPI/CurrentWeather.jsx
+++ src/components/WeatherAPI/CurrentWeather.jsx
@@ -3,10 +3,22 @@ const CurrentWeather = ({ currentWeather }) => {
   let icon = `http://openweathermap.org/img/wn/${currentWeather.icon}@2x.png`
   return (
     <div id="weather-display">
-      <div id= "icon"> <img src={icon} alt="icon"/>{currentWeather.weather} </div>
-      <div className="weather-info"> Conditions: <div> {currentWeather.conditions}</div> </div>
-      <div className="weather-info"> Temperature: <div> {currentWeather.temperature}&deg;C </div> </div>
-      <div className="weather-info"> Feels Like: <div> {currentWeather.feelslike}&deg;C</div> </div>
+      <div id= "icon"> 
+        <img src={icon} alt="icon"/>
+          <div id="temp">{currentWeather.weather}  </div> 
+       </div>
+      <div className="weather-info">  
+        Conditions:                 
+          <div> {currentWeather.conditions}</div> 
+      </div>
+      <div className="weather-info">
+         Temperature: 
+          <div id="temp"> Feels Like: </div> 
+      </div> 
+      <div className="weather-info">
+         {currentWeather.temperature}&deg;C 
+          <div id="temp"> {currentWeather.feelslike}&deg;C</div> 
+      </div>
     </div>
   );
 };
