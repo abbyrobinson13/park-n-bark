e9737b74716a32e93ffee468e52046254a79efdf
Abigail Robinson <abbyrobinson@shaw.ca>
2022-11-25 14:33:48 -0700

    added data table styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Main from "./components/Main";
 import NavBar from "./components/Navbar";
-import "react-big-calendar/lib/css/react-big-calendar.css"
-import "react-big-calendar/lib/addons/dragAndDrop/styles.css"
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Footer from "./components/Footer";
 import ScrollToTop from "./ScrollToTop";
+import "./components/DataTable/font-awesome-library.js";
 
 function App() {
   return (
