7b374f23e2eacf706ae03ce3420aac353e4c1bbc
Abigail Robinson <abbyrobinson@shaw.ca>
2022-11-25 14:35:33 -0700

    added font awesome symbols for table

---
+++ src/components/DataTable/font-awesome-library.js
@@ -0,0 +1,18 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faArrowUp,
+  faArrowDown,
+  faSort,
+  faSortUp,
+  faSortDown,
+  faArrowsUpDown,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faArrowUp,
+  faArrowDown,
+  faSort,
+  faSortUp,
+  faSortDown,
+  faArrowsUpDown
+);
