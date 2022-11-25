eb012a79a52bb3b99d2aee32336933809c602205
Abigail Robinson <abbyrobinson@shaw.ca>
2022-11-25 14:35:03 -0700

    updated page to add data table

--- src/pages/parks.jsx
+++ src/pages/parks.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Searchbox from "../components/Searchbox";
 import Weather from "../components/WeatherAPI/Weather";
 import { Map } from "../components/ParkMap/Map.jsx";
+import DataTable from "../components/DataTable/DataTable.jsx";
 
 const Parks = () => {
   return (
     <header className="App-header">
       <div id="parks-container">
-        <div id="park-display">Parks List</div>
+        <div id="park-display">
+          <DataTable className="data-table" />
+        </div>
         <div id="search-box">
           <Searchbox />
         </div>
