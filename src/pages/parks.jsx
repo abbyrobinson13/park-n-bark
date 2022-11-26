ad571e9bb22c11772b3b8e28782ad8cc9596ded9
Cobi Reeves <cobi.reeves@gmail.com>
2022-11-25 20:10:10 -0700

    park searchbar gone

--- src/pages/parks.jsx
+++ src/pages/parks.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Searchbox from "../components/Searchbox";
 import Weather from "../components/WeatherAPI/Weather";
 import { Map } from "../components/ParkMap/Map.jsx";
 import DataTable from "../components/DataTable/DataTable.jsx";
@@ -10,9 +9,6 @@ const Parks = () => {
       <div id="parks-container">
         <div id="park-display">
           <DataTable className="data-table" />
-        </div>
-        <div id="search-box">
-          <Searchbox />
         </div>
         <div id="map-display">
           <Map />
