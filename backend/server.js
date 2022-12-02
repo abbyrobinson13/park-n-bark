f4ee6e0a55afbc5b7cc1b292ceac1378446b920a
Abneet K Pabyal <Abneet.P@gmail.com>
2022-12-01 20:20:15 -0700

    Correct file setup

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ import express from "express";
 import dogFactRouter from "./routes/dog-facts-router.js";
 import weatherRouter from "./routes/weather-router.js";
 import parkRouter from "./routes/parkRoutes.js";
-import forecastRouter from "./routes/forecast-router.js";
+// import forecastRouter from "./routes/forecast-router.js";
+import expressOidc from "express-openid-connect";
 import eventRouter from "./routes/event-router.js";
+
+const { auth, requiresAuth } = expressOidc;
 
 dotenv.config();
 const app = express();
@@ -14,6 +17,32 @@ app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Web server running on port ${port}`);
+});
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SESSION_SECRET,
+  baseURL: process.env.OIDC_BASE_URL,
+  clientID: process.env.OIDC_CLIENT_ID,
+  issuerBaseURL: process.env.OIDC_ISSUER_BASE_URL,
+  routes: {
+    login: false,
+    callback: "/api/callback",
+    logout: "/api/logout",
+  },
+};
+app.use(auth(config));
+
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+app.get("/api/login", (req, res) => res.oidc.login({ returnTo: "/parks" }));
+
+app.get("/api/profile", requiresAuth(), (req, res) => {
+  const profile = req.oidc.user;
+  res.send(profile);
 });
 
 app.use("/api/facts", dogFactRouter);
