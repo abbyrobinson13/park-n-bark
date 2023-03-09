631244aaed5f1f42d90eb25ca5c8c541714bee89
Abigail Robinson <abbyrobinson@shaw.ca>
2023-03-09 12:30:20 -0700

    updated ml page

--- src/components/AI/AI.jsx
+++ src/components/AI/AI.jsx
@@ -1,39 +1,40 @@
 import React, { useRef, useState } from "react";
-import {drawImage} from 'canvas-object-fit';
+import { drawImage } from "canvas-object-fit";
 
 const AI = () => {
   const imageRef = useRef(null);
-  const canvRef = useRef(null)
+  const canvRef = useRef(null);
   const [imageUrl, setImageUrl] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [results, setResults] = useState([]);
 
   const startModel = async () => {
     setIsLoading(true);
-    await drawCanvas()
+    await drawCanvas();
     const ml5 = await import("ml5");
-    const classifier = await ml5.imageClassifier('./models/model.json');
+    const classifier = await ml5.imageClassifier("./models/model.json");
     const results = await classifier.classify(canvRef.current);
-    console.log('full results:', results)
+    console.log("full results:", results);
     setResults(results);
 
     setIsLoading(false);
   };
 
-  
-  const drawCanvas = async() => {
+  const drawCanvas = async () => {
     const canvas = canvRef.current;
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext("2d");
     const grayImage = new Image();
     grayImage.src = imageRef.current.src;
-   
+
     canvas.height = 400;
     canvas.width = 400;
     // ctx.filter = 'grayscale(1)';
-    drawImage(ctx, grayImage, 0,0, canvas.height, canvas.width, {objectFit: 'cover'});
-  }
+    drawImage(ctx, grayImage, 0, 0, canvas.height, canvas.width, {
+      objectFit: "cover",
+    });
+  };
 
-  const onFileChange = async(e) => {
+  const onFileChange = async (e) => {
     const file = e.target.files[0];
     const fileUrl = URL.createObjectURL(file);
     setImageUrl(fileUrl);
@@ -45,14 +46,21 @@ const AI = () => {
         <span>Upload dog image here to check breed!</span>
       </div>
       <input type="file" onChange={onFileChange} />
-      {imageUrl && ( <div id="ai-image-holder">
-        <img ref={imageRef} src={imageUrl} id="ai-image" alt="Preview" width='400px' height='400px'/>
-        <canvas ref={canvRef} />
+      {imageUrl && (
+        <div id="ai-image-holder">
+          <img
+            ref={imageRef}
+            src={imageUrl}
+            id="ai-image"
+            alt="Preview"
+            width="400px"
+            height="400px"
+          />
+          <canvas ref={canvRef} />
         </div>
       )}
       {imageUrl && <button onClick={startModel}>Start</button>}
       {isLoading && <div>Loading...</div>}
-      
 
       {results.length > 0 && (
         <table>
